37be5f39c09bea23b8ccc747ababff846eacf2ba
s-as-as100 <s.arifsaifi@gmail.com>
2021-07-01 20:20:51 +0530

    calculate the pair sum is zero using one loop for optimal solution

--- SumZero.js
+++ SumZero.js
@@ -4,23 +4,44 @@
 // output -- [-4,4]
  
 
-function getSumPairZero(array) {
-    for(let number of array) {
-        console.log("outer loop") 
-        for(let j=1 ;j <array.length; j++) {
-            console.log("inner loop") 
-            if(number + array[j] ===0) {
-                return [number,array[j]]
-            }
-        }
+// function getSumPairZero(array) {
+//     for( let number of array) {
+//         console.log(number)
+
+//         for(j=1; j< array.length ; j++) {
+//             if(number + array[j] === 0) {
+//                 return[number,array[j]]
+//             }
+//         }
+//     }
+// }
 
 
-        console.log(number)
-    }
-}
+// const result =getSumPairZero([-5,-4,-3,-2,0,2,4,6,8])
 
-const result=getSumPairZero([-5,-4,-3,-2,0,2,4,6,8] );
- console.log(result)
-
+// console.log(result)
  // o(n^2) quadratic the complexity
- 
+
+
+ // Optimized Solutions for sumpairZero
+
+ function FindPair(array) {
+     let left = 0;
+     let right = array.length-1 ;
+      while(right > left) {
+          sum = array[left] + array[right];
+          if ( array[left] + array[right] === 0 ) {
+              return [ array[right], array[left] ]
+          } else if(sum > 0) {
+              right--;
+          }
+          else {
+              left++;
+          }
+      }
+
+ }
+
+ const result =FindPair([-5,-4,-3,-2,0,2,4,6,8])
+
+console.log(result)
